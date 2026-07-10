f759c5f9ae47029f9ac1ea49de2b514e7259a68e
sitepass-js <sitepass@nate.com>
2026-07-10 23:15:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SITEPASS_SW_VERSION = 'v23.7.402';
-const SITEPASS_CACHE = 'sitepass-v23.7.402';
+const SITEPASS_SW_VERSION = 'v23.7.405';
+const SITEPASS_CACHE = 'sitepass-v23.7.405';
 const SITEPASS_CORE = [
   './',
   './index.html',
